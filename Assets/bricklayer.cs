082c23d465bd95bc55ecad1a69d70bbc6d6cc21b
Brett Ian Balogh <brettbalogh@gmail.com>
2017-01-16 15:28:17 -0600

    init

---
+++ Assets/bricklayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bricklayer : MonoBehaviour {
+	public GameObject brick;
+	// Use this for initialization
+	void Start () {
+		for (int n = 0; n < 1000; n++) {
+				float posX = Random.Range (0, 1000);
+				float posY = Random.Range (0, 10);
+				float posZ = Random.Range (0, 1000);
+				Instantiate(brick, new Vector3(posX, posY, posZ), Quaternion.identity);
+				Rigidbody brickBody = brick.AddComponent<Rigidbody> ();
+				brickBody.mass = 1.0f;
+			}
+		}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
