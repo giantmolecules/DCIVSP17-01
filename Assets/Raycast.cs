c26007d3318ba62eafa9344a6c36fed8168dfe02
Brett Ian Balogh <brettbalogh@gmail.com>
2017-01-16 20:19:40 -0600

    commit

---
+++ Assets/Raycast.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	public class Raycast : MonoBehaviour 
+	{
+		void FixedUpdate() 
+		{
+			Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+			if (Physics.Raycast(transform.position, fwd)) 
+				print("There is something in front of the object!");
+		}
+	}
