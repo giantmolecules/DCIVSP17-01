c1e6a53cc35a37e71fe14ac40f5a8c2fbb8b39e0
Brett Ian Balogh <brettbalogh@gmail.com>
2017-01-30 18:59:55 -0600

    commit

--- Assets/Scripts/EVENTS/Object1Script.cs
+++ Assets/Scripts/EVENTS/Object1Script.cs
@@ -1,15 +1,47 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using System.Collections;
 
 public class Object1Script : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private UnityAction someListener;
+	private Renderer r;
+
+	void Awake ()
+	{
+		r = GetComponent<Renderer> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnEnable ()
+	{
+		EventManager.StartListening ("state01", SomeFunction);
+		EventManager.StartListening ("state02", SomeOtherFunction);
+		EventManager.StartListening ("state03", SomeThirdFunction);
+	}
+
+	void OnDisable ()
+	{
+		EventManager.StopListening ("state01", SomeFunction);
+		EventManager.StopListening ("state02", SomeOtherFunction);
+		EventManager.StopListening ("state03", SomeThirdFunction);
+	}
+
+	void SomeFunction ()
+	{
+		Debug.Log ("State 1 was called!");
+
+		r.material.color = new Color (0, 0, 255);
+	}
+
+	void SomeOtherFunction ()
+	{
+		Debug.Log ("State 2 was called!");
+		r.material.color = new Color (255, 0, 0);
+	}
+
+	void SomeThirdFunction ()
+	{
+		Debug.Log ("State 3 was called!");
+		r.material.color = new Color (0, 255, 0);
 	}
 }
