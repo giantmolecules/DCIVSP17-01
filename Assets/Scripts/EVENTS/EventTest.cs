30f1183dce5c8a2ca25532624a362584baf6ffbf
Brett Ian Balogh <brettbalogh@gmail.com>
2017-01-31 06:32:20 -0600

    commit

--- Assets/Scripts/EVENTS/EventTest.cs
+++ Assets/Scripts/EVENTS/EventTest.cs
@@ -6,37 +6,42 @@ public class EventTest : MonoBehaviour {
 
 	private UnityAction someListener;
 
-	void Awake ()
+	void Start ()
 	{
 		//someListener = new UnityAction (SomeFunction);
+		//hello
 	}
 
 	void OnEnable ()
 	{
-		EventManager.StartListening ("state01", SomeFunction);
-		EventManager.StartListening ("state02", SomeOtherFunction);
-		EventManager.StartListening ("state03", SomeThirdFunction);
+		EventManager.StartListening ("state01", StateFunction01);
+		EventManager.StartListening ("state02", StateFunction02);
+		EventManager.StartListening ("state03", StateFunction03);
 	}
 
 	void OnDisable ()
 	{
-		EventManager.StopListening ("state01", SomeFunction);
-		EventManager.StopListening ("state02", SomeOtherFunction);
-		EventManager.StopListening ("state03", SomeThirdFunction);
+		EventManager.StopListening ("state01", StateFunction01);
+		EventManager.StopListening ("state02", StateFunction02);
+		EventManager.StopListening ("state03", StateFunction03);
 	}
 
-	void SomeFunction ()
+	void StateFunction01 ()
 	{
 		Debug.Log (gameObject.name + "Reporting State 1 was called!");
 	}
 
-	void SomeOtherFunction ()
+	void StateFunction02 ()
 	{
 		Debug.Log (gameObject.name + "Reporting State 2 was called!");
 	}
 
-	void SomeThirdFunction ()
+	void StateFunction03 ()
 	{
 		Debug.Log (gameObject.name + "Reporting State 3 was called!");
+	}
+
+	void Update(){
+
 	}
 }
