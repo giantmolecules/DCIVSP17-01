f35545076b6448fd611365455d7f0c8a49927c06
Brett Ian Balogh <brettbalogh@gmail.com>
2017-02-10 08:29:42 -0600

    commit

--- Assets/Scripts/CAWeather.cs
+++ Assets/Scripts/CAWeather.cs
@@ -3,11 +3,16 @@ using System.Collections;
 
 public class CAWeather : MonoBehaviour {
 
-	public Renderer rend;
+	private Renderer rend;
+	private bool[] neighbors;
 
 	// Use this for initialization
 	void Start () {
 		rend = GetComponent<Renderer>();
+		neighbors = new bool[16];
+		for (int i = 0; i < neighbors.Length; i++) {
+			neighbors [i] = Random.Range (0, 2);
+		}
 	}
 	
 	// Update is called once per frame
